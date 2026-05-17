c7cfc1c87dce6126ed9f982eec98ffaa76f9da64
Mahetab-Dagher <mahetab.dagher@gmail.com>
2026-05-18 02:13:35 +0300

    adding countdown timer

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
+		int remainingSeconds = 180;
+
 		Scanner sc = new Scanner(System.in);
 		Board b = new Board();
 		b.initBoard();
@@ -18,7 +20,14 @@ public class Main {
 		String prev="";
 		String curr="";
 		int prev_r = -1 ,prev_c = -1;
-		while(!b.isComplete()) {
+		while(!b.isComplete() && remainingSeconds > 0) {
+			System.out.println("Time Left: " + remainingSeconds + " s.");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Interrupted while waiting");
+            }
+            remainingSeconds--;
 			System.out.println("--- Which card do you wish to flip? ---");
 			System.out.println("--- Enter row col (i.g 2 3) ---");
 			
@@ -55,6 +64,14 @@ public class Main {
 			}
 			
 			
+		}
+		if(remainingSeconds<=0 && !b.isComplete()) {
+			System.out.println("---Game Over 😭---");
+			System.out.println("---Time out 🕙---");
+			
+		}
+		else {
+			System.out.println("Conguratulations!! You Won 🎉🎉🎉");
 		}
 	}
 
